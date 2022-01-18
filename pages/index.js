b4a83729677d29661c955068a3504fe01c0a1988
Eddie <elremolino91@gmail.com>
2022-01-18 10:58:25 -0800

    changed about component to services

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Intro from './intro.js';
-import About from './about.js';
+import Services from './services.js';
 
 export default function Home() {
   return (
    <div className="bg-slate-500 min-h-screen">
-     <Intro />
-    <About />
+    <Intro />
+    <Services />
     <section className=" border-yellow-500 border-4 min-w-screen">About Me</section>
     <footer>call me beep me if ya wanna reach me</footer>
    </div>
--- pages/services.js
+++ pages/services.js
@@ -1,9 +1,9 @@
 
-const About = () => {
+const Services = () => {
   return (
     <section className="border-rose-500 border-4 min-w-screen">What I do
     </section>
   )
 }
 
-export default About;
+export default Services;
