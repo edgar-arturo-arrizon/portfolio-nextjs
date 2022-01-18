800119999f911edf6ea4dddf9e37118b8bf0eb24
Eddie <elremolino91@gmail.com>
2022-01-18 13:58:45 -0800

    work section finished

--- pages/work.js
+++ pages/work.js
@@ -1,7 +1,24 @@
 const Work = () => {
   return (
-    <section className=" bg-stone-800 border-4 min-w-screen px-10 py-8  text-center sm:grid sm:grid-cols-2">
-      tbd
+    <section className="bg-stone-800 border-4 min-w-screen px-10 py-8  text-center">
+      <h2 className=" text-white text-5xl font-bold py-3">My Work</h2>
+      <p className="text-3xl font-medium text-green-400 py-2 sm:col-start-1">In progress..</p>
+
+      <div className="grid grid-flow-row gap-8  lg:grid-flow-col">
+        <a href="">
+          <img src="portfolio-01.jpg" />
+        </a>
+        <a className="overflow-hidden" href="">
+          <img src="portfolio-02.jpg" />
+        </a>
+        <a className="overflow-hidden" href="">
+          <img src="portfolio-03.jpg" />
+        </a>
+        <a className="overflow-hidden" href="">
+          <img src="portfolio-04.jpg" />
+        </a>
+      </div>
+
     </section>
   )
 }
