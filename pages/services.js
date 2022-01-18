3010d62830fbd3cf94e1b97fd2f3e135e0441d56
Eddie <elremolino91@gmail.com>
2022-01-18 11:45:43 -0800

    mobile view of Services component done

--- pages/services.js
+++ pages/services.js
@@ -1,8 +1,22 @@
 
 const Services = () => {
   return (
-    <section className="border-rose-500 border-4 min-w-screen bg-imag bg-services">What I do
-    
+    <section className="border-4 min-w-screen px-20 py-8 bg-imag bg-services text-center">
+      <h2 className="text-green-400 py-8 text-6xl font-semibold">What I Do</h2>
+      <div className="text-white mb-16 ">
+        <div className="m-auto py-6">
+          <h3 className="text-2xl py-4 font-bold">Frontend Design</h3>
+          <p className="text-lg md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className="m-auto py-4">
+          <h3 className="text-2xl py-4 font-bold">Backend Experience</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className="m-auto py-4">
+          <h3 className="text-2xl py-4 font-bold">Rest API Developement</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
     </section>
   )
 }
