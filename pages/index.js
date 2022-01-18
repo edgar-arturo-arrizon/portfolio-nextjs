efede2707616a2bfc203650c5f8da9cfef53396c
Eddie <elremolino91@gmail.com>
2022-01-17 22:28:03 -0800

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Intro from './intro.js';
 
 export default function Home() {
   return (
-    <>
-    hello
-    </>
+   <div className="bg-slate-500 min-h-screen flex-col">
+     <Intro />
+    <section className="border-rose-500 border-4 min-w-screen">What I do</section>
+    <section className=" border-yellow-500 border-4 min-w-screen">About Me</section>
+   </div>
   )
 }
---
+++ pages/intro.js
@@ -0,0 +1,12 @@
+
+const Intro = () => {
+  return (
+    <>
+      <section className="border-cyan-500 border-4 min-w-screen px-20 py-8">
+        Intro
+      </section>
+    </>
+  )
+}
+
+export default Intro;
