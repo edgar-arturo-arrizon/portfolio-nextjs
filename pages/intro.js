97ea9c4c402ce215f1baf9b25931184e23cf9729
Eddie <elremolino91@gmail.com>
2022-01-18 10:48:59 -0800

    Intro section complete

--- pages/intro.js
+++ pages/intro.js
@@ -1,11 +1,17 @@
+import Image from 'next/image'
+import introPic from '../public/intro.jpeg'
 
 const Intro = () => {
   return (
-    <>
-      <section className="border-cyan-500 border-4 min-w-screen px-20 py-8">
-        Intro
-      </section>
-    </>
+    <section className="border-white-500 border-4 px-20 py-8 md:bg-white sm:bg-white flex flex-row  justify-center">
+      <div className="relative top-20 mx-7">
+        <h1 className="text-5xl">
+          Hi, I am <strong>Edgar Arrizon</strong>
+        </h1>
+        <p className="text-3xl bg-green-400 md:w-[400px] md:inline">Fullstack developer</p>
+      </div>
+      <img src="intro.jpeg" className="w-80" alt="Picture of the author"></img>
+    </section>
   )
 }
 
