47feb3d9a8de4698b18ec954e88610b62b320ba7
Eddie <elremolino91@gmail.com>
2022-01-18 13:03:27 -0800

    made work component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Intro from './intro.js';
 import Services from './services.js';
 import About from './about.js';
+import Work from './work.js';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Intro />
     <Services />
     <About />
+    <Work />
     <footer>call me beep me if ya wanna reach me</footer>
    </div>
   )
---
+++ pages/work.js
@@ -0,0 +1,9 @@
+const Work = () => {
+  return (
+    <section className="bg-grey border-4 min-w-screen px-10 py-8  text-center sm:grid sm:grid-cols-2">
+      tbd
+    </section>
+  )
+}
+
+export default Work;
