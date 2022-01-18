5d388613f571385ce2807ba4f1c7857f44833ded
Eddie <elremolino91@gmail.com>
2022-01-18 13:11:12 -0800

    finished about me section

--- pages/work.js
+++ pages/work.js
@@ -1,6 +1,6 @@
 const Work = () => {
   return (
-    <section className="bg-grey border-4 min-w-screen px-10 py-8  text-center sm:grid sm:grid-cols-2">
+    <section className=" bg-stone-800 border-4 min-w-screen px-10 py-8  text-center sm:grid sm:grid-cols-2">
       tbd
     </section>
   )
