088f02a25b7f2e1d056bddc5b21fb6966d7ca43c
Eddie <elremolino91@gmail.com>
2022-01-18 12:18:02 -0800

    forgot to make branch

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+const About = () => {
+  return (
+    <section className=" border-yellow-500 border-4 min-w-screen">
+      About Me
+    </section>
+  )
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Intro from './intro.js';
 import Services from './services.js';
+import About from './About.js';
 
 export default function Home() {
   return (
    <div className="bg-slate-500 min-h-screen">
     <Intro />
     <Services />
-    <section className=" border-yellow-500 border-4 min-w-screen">About Me</section>
+    <About />
     <footer>call me beep me if ya wanna reach me</footer>
    </div>
   )
