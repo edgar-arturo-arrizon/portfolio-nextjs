23ced04f6e5fb234e6f57f1d0418cf8a5bc3bd32
Eddie <elremolino91@gmail.com>
2022-01-18 10:55:23 -0800

    made About component

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+
+const About = () => {
+  return (
+    <section className="border-rose-500 border-4 min-w-screen">What I do
+    </section>
+  )
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Intro from './intro.js';
+import About from './about.js';
 
 export default function Home() {
   return (
    <div className="bg-slate-500 min-h-screen">
      <Intro />
-    <section className="border-rose-500 border-4 min-w-screen">What I do</section>
+    <About />
     <section className=" border-yellow-500 border-4 min-w-screen">About Me</section>
     <footer>call me beep me if ya wanna reach me</footer>
    </div>
