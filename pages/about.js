a4761be2a9a50970102228feb4b32734cc6dbb15
Eddie <elremolino91@gmail.com>
2022-01-19 14:44:38 -0800

    reverted back to img tags

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
         <p className="text-lg lg:text-xl py-3">From personal trainer to software developer! I started coding in hopes of building some of the applications I felt were needed by coaches and wellness professionals. I&apos;m currently using Next.js, Tailwind and MongoDB to bring my ideas to life!</p>
         <p className="text-lg lg:text-xl py-3">Having attended an immersive program for fullstack developement. Where I was trained and able to develope my skills further I feel ready to become a member of a professional team.</p>
       </div>
-      <Image src="about_me.jpeg" className="max-w-md sm:relative sm:left-5 sm:bottom-36 sm:pt-10 " alt="Picture of the author"></Image>
+      <img src="/about_me.jpeg"  className="max-w-md sm:relative sm:left-5 sm:bottom-36 sm:pt-10 " alt="Picture of the author"></img>
     </section>
   )
 }
