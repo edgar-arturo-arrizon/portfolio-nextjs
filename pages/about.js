1b39e6de47ec1641ce1cc981ba9f43fc7bcbb68d
Eddie <elremolino91@gmail.com>
2022-01-19 14:42:13 -0800

    another &apos issue in About

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ const About = () => {
       <p className="text-3xl font-medium bg-green-400 text-left py-2 sm:col-start-1">Fullstack developer based out of Oakland, CA</p>
 
       <div className="text-left sm:col-start-1 ">
-        <p className="text-lg lg:text-xl py-3">From personal trainer to software developer! I started coding in hopes of building some of the applications I felt were needed by coaches and wellness professionals. I'm currently using Next.js, Tailwind and MongoDB to bring my ideas to life!</p>
+        <p className="text-lg lg:text-xl py-3">From personal trainer to software developer! I started coding in hopes of building some of the applications I felt were needed by coaches and wellness professionals. I&apos;m currently using Next.js, Tailwind and MongoDB to bring my ideas to life!</p>
         <p className="text-lg lg:text-xl py-3">Having attended an immersive program for fullstack developement. Where I was trained and able to develope my skills further I feel ready to become a member of a professional team.</p>
       </div>
       <Image src="about_me.jpeg" className="max-w-md sm:relative sm:left-5 sm:bottom-36 sm:pt-10 " alt="Picture of the author"></Image>
